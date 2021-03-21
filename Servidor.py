7d86784a97bcb41c35899acb25281bbc2320faee
William Soares <williamoliveirafsa@outlook.com>
2021-03-20 22:51:25 -0300

    adding some features

--- Servidor.py
+++ Servidor.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import mercury
 
 # An extremely simple HTTP server
-import socket, sys, time
+import socket, sys, time, os
 # Server runs on all IP addresses by default
 HOST=''
 #HOST = 'localhost'
@@ -24,11 +24,11 @@ except socket.error :
 
 s.listen(1)
 
-reader = mercury.Reader("tmr:///dev/ttyUSB0", baudrate=230400)
-reader.set_region("NA2")
-reader.set_read_plan([1], "GEN2", read_power=1500)
+#reader = mercury.Reader("tmr:///dev/ttyUSB0", baudrate=230400)
+#reader.set_region("NA2")
+#reader.set_read_plan([1], "GEN2", read_power=1500)
 #print(reader.read())
-
+'''
 tags = list(map(lambda t: t.epc, reader.read()))
 array = []
 
@@ -36,21 +36,33 @@ for x in range(len(tags)):
     dado = str(tags[x])
     print(dado)
     array.append(dado+"\n")
-
+'''
 while True:
 	con, cliente = s.accept()
 	print ('Concetado por', cliente)
+	flname = 'arquivos\\configInicial.txt'
+	arq = open(flname,'a')
 	while True:
-		msg = con.recv(1024)
-		if not msg: break
-		#print (msg.decode("utf-8"))
-		print (msg)
-		for y in range(len(tags)):
-			msg2 = str.encode(array[y])
-			type(msg2)
-			print(array[y])
-			con.send(msg2)
+		print ('Iniciando...')
+		recb = con.recv(5120)
+		#if not recb: break
+		print(recb)
+		#msg = str.encode(recb, 'utf-8')
+		msg = recb.decode('utf-8')
+		print(msg)
+		#arq.write(str.decode(con.recv(5120),'UTF-8'))
+		arq.write(msg)
+		print ('Arquivo enviado!')
+		#msg = con.recv(5120)
+		#if not recb: break
+		#print (msg)
+		#for y in range(len(tags)):
+		#	msg2 = str.encode(array[y])
+		#	type(msg2)
+		#	print(array[y])
+		#	con.send(msg2)
 			#for z in range(5):
 				#print(z)
+	arq.close()
 	print ('Finalizando conexao do cliente', cliente)
 	con.close()
